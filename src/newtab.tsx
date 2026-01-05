a53b1e66dca4f4ddca6a4047957e1927f534fd68
Вавилихин Владислав Русланович <v.vavilikhin@2gis.ru>
2026-01-05 12:09:37 +0300

    wip: plasmo clerk dev setup

--- src/newtab.tsx
+++ src/newtab.tsx
@@ -11,25 +11,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Clerk Publishable Key. Add PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to .env')
 }
 
-// Warn if sync host is not configured (OAuth won't work without it)
-if (!SYNC_HOST) {
-  console.warn(
-    '[Clerk] PLASMO_PUBLIC_CLERK_SYNC_HOST not configured. OAuth providers (GitHub, Google) will not work.'
-  )
-}
-
 // Chrome Extension URL for navigation redirects (newtab page)
-// Use getURL directly with the page name to avoid double-slash issues
-const NEW_TAB_URL = `${chrome.runtime.getURL('.')}newtab.html`
+const EXTENSION_URL = chrome.runtime.getURL('.')
 
 function NewTabPage() {
   return (
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
-      syncHost={SYNC_HOST}
-      afterSignOutUrl={NEW_TAB_URL}
-      signInFallbackRedirectUrl={NEW_TAB_URL}
-      signUpFallbackRedirectUrl={NEW_TAB_URL}
+      afterSignOutUrl={`${EXTENSION_URL}/newtab.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/newtab.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/newtab.html`}
     >
       <App />
     </ClerkProvider>
